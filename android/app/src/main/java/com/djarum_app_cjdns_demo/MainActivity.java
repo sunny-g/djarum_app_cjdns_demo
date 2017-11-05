fa2253d1b3ccb244aaacb973631e37008ed4ca11
Sunny G <sunny.gonna@gmail.com>
2017-11-04 19:07:49 -0700

    initial commit

---
+++ android/app/src/main/java/com/djarum_app_cjdns_demo/MainActivity.java
@@ -0,0 +1,15 @@
+package com.djarum_app_cjdns_demo;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+    /**
+     * Returns the name of the main component registered from JavaScript.
+     * This is used to schedule rendering of the component.
+     */
+    @Override
+    protected String getMainComponentName() {
+        return "djarum_app_cjdns_demo";
+    }
+}
